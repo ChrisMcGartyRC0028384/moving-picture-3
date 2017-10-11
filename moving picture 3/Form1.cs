dcb4b7679e1a3884f0201190c83040e0b2115335
rc0028384 <rc0028384@CRO-ICF07-24>
2017-10-11 15:46:47 +0100

    Add project files.

---
+++ moving picture 3/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace moving_picture_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            while (pictureBox1.Location.X + 100 < ClientRectangle.Width - pictureBox1.Width)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 100, pictureBox1.Location.Y);
+                await Task.Delay(500);
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y - 100);
+                await Task.Delay(500);
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y + 100);
+                await Task.Delay(500);
+                //pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 100);
+                //await Task.Delay(500);
+            }
+        }
+    }
+}
