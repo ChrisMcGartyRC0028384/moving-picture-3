294286d0451402e9eeceffaa9b091cc67240501f
rc0028384 <rc0028384@CRO-ICF07-24>
2017-11-22 13:27:38 +0000

    NEW BUTTON

--- moving picture 3/Form1.cs
+++ moving picture 3/Form1.cs
@@ -31,5 +31,24 @@ namespace moving_picture_3
                 //await Task.Delay(500);
             }
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 100, pictureBox1.Location.Y);
+                await Task.Delay(500);
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 50, pictureBox1.Location.Y + 100);
+                await Task.Delay(500);
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 50, pictureBox1.Location.Y - 100);
+                //pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 100);
+                //await Task.Delay(500);
+            }
+        }
     }
 }
